75bb6a7f4de65e94add27318a73cc92b59d7503d
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-28 15:42:32 -0500

    add: test routes, fix: timestamp field in model

--- server/models/joke.model.js
+++ server/models/joke.model.js
@@ -2,16 +2,21 @@ const mongoose = require("mongoose");
 
 // Creates a collection in the MongoDB with below format
 const JokeSchema = new mongoose.Schema({
-    name: {
+    prompt: {
         type: String,
-        required: [true, "Joke is required."],
-        minlength: [10, "Jokes must be at least 10 characters in length."]
+        required: [true, "Joke is required!"],
+        minlength: [10, "Jokes must be at least 10 characters in length!"]
+    },
+    punchline: {
+        type: String,
+        required: [true, "Must enter a punchline!"],
+        minlength: [1, "Punchline must have at least one character!"]
     },
     isFavorite:{
         type: Boolean,
         default: false
     }
-}, {timestamp:true});
+}, {timestamps:true});
 
 
 // Creates instance of a Joke collection (i.e. document)
